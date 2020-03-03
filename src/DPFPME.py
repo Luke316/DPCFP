030ddca9c17b3e80fb8f41ebf98f9f07e21720ac
Luke316 <yumluhwang@gmail.com>
2020-03-03 21:38:49 +0800

    20200303_MISTable_done

---
+++ src/DPFPME.py
@@ -0,0 +1,20 @@
+from time import time
+
+class FPMEtreeNode():
+    def __init__(self,name,MIS,sup):
+        self.name = None
+        self.MIS = None
+        self.sup = None
+    def setLeft(self, left):
+        self.left = left
+    def setRight(self, right):
+        self.right = right 
+
+
+def FPSpanning :
+
+    return 
+
+
+if __name__ == '__main__': 
+    
--- src/preprocess_phase.py
+++ src/preprocess_phase.py
@@ -1,47 +1,79 @@
 from collections import OrderedDict
 from random import sample
 import numpy as np
-from dataset_processing import DifferentItemsCount, TruncateDatabase
+from dataset_processing import DifferentItemsCount, TruncateDatabase, ReadDataset
+from time import time
+from pprint import pprint
+import csv
 
 #from dataset_processing import init_T
 
-# Assign MIS not finished
-# def MISAssign(D, beta, threshold, epsilon, total_items):
-#     items = DifferentItemsCount(D)
-#     for transaction in D:
+# def NoisySupportCount(T, items,n,epsilon,truncated_length):
+#     #init support with 0
+#     support = dict.fromkeys(items, 0) 
+
+#     #scan dataset to get origin support
+#     for transaction in T:
 #         for item in transaction:
-#             if 
+#             if item in items:
+#                 support[item]=support.get(item) + 1    
+#     #pprint (support)
+#     # with open('origin_support.csv', 'w') as f:
+#     #     for key in support.keys():
+#     #         f.write("%s,%s\n"%(key,support[key]))
 
+#     # can use the same dictionary if need to reduce memory
+#     #sensitivity = truncated_length/n
+#     for item in support.keys():
+#         #support[item] = support.get(item)/n + np.random.laplace(0, sensitivity / epsilon)
+#         noisy_support[item] = noisy_support.get(item) + np.random.laplace(0, truncated_length/ epsilon)
+#     # with open('noisy_support.csv', 'w') as f:
+#     #     for key in noisy_support.keys():
+#     #         f.write("%s,%s\n"%(key,noisy_support[key]))
 
+#     sorted_noisy_support={k: v for k, v in sorted(noisy_support.items(), key=lambda item: item[1],reverse = True)}
+#     # with open('sorted_noisy_support.csv', 'w') as f:
+#     #     for key in sorted_noisy_support.keys():
+#     #         f.write("%s,%s\n"%(key,sorted_noisy_support[key]))
 
-#     I_truncated_counts = OrderedDict()
-#     for transaction in T_theta:
-#         for item in transaction:
-#             I_truncated_counts[frozenset([item])] = I_truncated_counts.get(frozenset([item]), 0) + 1
+#     #pprint (sorted_support)
+#     return sorted_noisy_support, support
 
-#     MS = OrderedDict()
-#     sensitivity = theta / n
-#     for itemset in I_truncated_counts.keys():  # E.g. itemset = frozenset({1}) rather than item = 1
-#         truncated_support = 1/n * I_truncated_counts[itemset]
-#         noisy_truncated_support = truncated_support + np.random.laplace(0, sensitivity / ep_a)
-#         noisy_truncated_support = threshold(noisy_truncated_support)
-#         MS[itemset] = max(rho * noisy_truncated_support, lam)
-#     time_used = time() - time_start
-#     print('DPARM: Assigned MIS. Running time: {:.3f} seconds.'.format(time_used))
-
-#     return MS
-
-def SupportCount(T, items):
+def MISTable(T, items,n,epsilon,truncated_length,threshold, beta): #beta in DPARM = 0.25
+    #init support with 0
     support = dict.fromkeys(items, 0) 
 
+    #scan dataset to get origin support
     for transaction in T:
         for item in transaction:
             if item in items:
-                support[item]=support.get(item) + 1
-    print (support)
-    return support
+                support[item]=support.get(item) + 1    
+
+    sensitivity = truncated_length/n
+    MIS_table = dict.fromkeys(items, 0)
+    frequent_items = dict.fromkeys(items,1.1)
+    for item in support.keys():
+        support[item] = support.get(item)/n + np.random.laplace(0, sensitivity / epsilon)
+        MIS_table[item] = max(beta*support[item],threshold) #if beta =0, the MIS is basically threshold and equals to FIM
+        if (support[item] >= MIS_table[item]):
+            #print('the support is bigger than MIS',item,'sup',support[item],'MIS',MIS_table[item])
+            frequent_items[item] = support[item]
+
+    # sort the frequent item in ascending order
+    sorted_frequent_items={k: v for k, v in sorted(frequent_items.items(), key=lambda item: item[1])}
+
+    # test
+    # pprint (sorted_frequent_items)
+    # print('sorted frequent items =',len(sorted_frequent_items))
+    # with open('sorted_frequent_items.csv', 'w') as f:
+    #     for key in sorted_frequent_items.keys():
+    #         f.write("%s,%s\n"%(key,sorted_frequent_items[key]))
+
+
+    return sorted_frequent_items, MIS_table
 
 if __name__ == '__main__': #if file wasn't imported.
-    truncatedT, items = TruncateDatabase('T10I4D100K',0.05)
-    SupportCount(truncatedT,items)
+    T, n = ReadDataset('T10I4D100K')
+    truncatedT, items, truncated_length = TruncateDatabase(T,0.05,n)
+    MISTable(truncatedT,items,n,1,truncated_length,0.01,0.25)
     print('preprocess')
