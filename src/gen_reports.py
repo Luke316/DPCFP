db4d6ab86da9d4997cce1ee38d0ad6b6cc937b1e
Luke316 <yumluhwang@gmail.com>
2020-04-22 17:25:35 +0800

    20200422_rename

--- src/gen_reports.py
+++ src/gen_reports.py
@@ -18,7 +18,7 @@ def plot_metrics(T_names, privacy, utility):
     fig4, ax4 = plt.subplots(figsize=(6, 6))
     fig5, ax5 = plt.subplots(figsize=(6, 6))
 
-    ax1.set_xlabel('Privacy Loss')
+    ax1.set_xlabel('Privacy Budget')
     ax1.set_ylabel('Precision')
     ax1.set_xlim(1, 5)
     ax1.set_ylim(0, 1)
@@ -26,7 +26,7 @@ def plot_metrics(T_names, privacy, utility):
     ax1.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
     ax1.grid(True, ls='--')
 
-    ax2.set_xlabel('Privacy Loss')
+    ax2.set_xlabel('Privacy Budget')
     ax2.set_ylabel('Recall')
     ax2.set_xlim(1, 5)
     ax2.set_ylim(0, 1)
@@ -34,7 +34,7 @@ def plot_metrics(T_names, privacy, utility):
     ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
     ax2.grid(True, ls='--')
 
-    ax3.set_xlabel('Privacy Loss')
+    ax3.set_xlabel('Privacy Budget')
     ax3.set_ylabel('F Score')
     ax3.set_xlim(1, 5)
     ax3.set_ylim(0, 1)
@@ -42,7 +42,7 @@ def plot_metrics(T_names, privacy, utility):
     ax3.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
     ax3.grid(True, ls='--')
 
-    ax4.set_xlabel('Privacy Loss')
+    ax4.set_xlabel('Privacy Budget')
     ax4.set_ylabel('MAE')
     ax4.set_xlim(1, 5)
     ax4.set_ylim(0, 0.025)
@@ -50,7 +50,7 @@ def plot_metrics(T_names, privacy, utility):
     ax4.yaxis.set_major_locator(ticker.MultipleLocator(0.0025))
     ax4.grid(True, ls='--')
 
-    ax5.set_xlabel('Privacy Loss')
+    ax5.set_xlabel('Privacy Budget')
     ax5.set_ylabel('MRE')
     ax5.set_xlim(1, 5)
     ax5.set_ylim(0, 0.15)
@@ -195,296 +195,7 @@ def gen_txt(T_names, privacy, utility):
             pu.write('\n\n')
 
 
-def plot_metrics_2(T_names, r_lst, utility):
-    fig1, ax1 = plt.subplots(figsize=(6, 6))
-    fig2, ax2 = plt.subplots(figsize=(6, 6))
-    fig3, ax3 = plt.subplots(figsize=(6, 6))
-    fig4, ax4 = plt.subplots(figsize=(6, 6))
-    fig5, ax5 = plt.subplots(figsize=(6, 6))
 
-    ax1.set_xlabel('Split Rate')
-    ax1.set_ylabel('Precision')
-    ax1.set_xlim(0.05, 0.5)
-    ax1.set_ylim(0.0, 1)
-    ax1.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
-    ax1.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax1.grid(True, ls='--')
-
-    ax2.set_xlabel('Split Rate')
-    ax2.set_ylabel('Recall')
-    ax2.set_xlim(0.05, 0.5)
-    ax2.set_ylim(0, 1)
-    ax2.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax2.grid(True, ls='--')
-
-    ax3.set_xlabel('Split Rate')
-    ax3.set_ylabel('F Score')
-    ax3.set_xlim(0.05, 0.5)
-    ax3.set_ylim(0, 1)
-    ax3.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
-    ax3.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax3.grid(True, ls='--')
-
-    ax4.set_xlabel('Split Rate')
-    ax4.set_ylabel('MAE')
-    ax4.set_xlim(0.05, 0.5)
-    ax4.set_ylim(0, 0.025)
-    ax4.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
-    ax4.yaxis.set_major_locator(ticker.MultipleLocator(0.0025))
-    ax4.grid(True, ls='--')
-
-    ax5.set_xlabel('Split Rate')
-    ax5.set_ylabel('MRE')
-    ax5.set_xlim(0.05, 0.5)
-    ax5.set_ylim(0, 0.15)
-    ax5.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
-    ax5.yaxis.set_major_locator(ticker.MultipleLocator(0.015))
-    ax5.grid(True, ls='--')
-
-    for T_name in T_names:
-        precision_mean, precision_std = [], []
-        recall_mean, recall_std = [], []
-        f_score_mean, f_score_std = [], []
-
-        mae_mean, mae_std = [], []
-        mre_mean, mre_std = [], []
-
-        for i in range(len(r_lst)):
-            precision_mean.append(np.mean(utility[T_name]['precision'][i]))
-            precision_std.append(np.std(utility[T_name]['precision'][i]))
-
-            recall_mean.append(np.mean(utility[T_name]['recall'][i]))
-            recall_std.append(np.std(utility[T_name]['recall'][i]))
-
-            f_score_mean.append(np.mean(utility[T_name]['f_score'][i]))
-            f_score_std.append(np.std(utility[T_name]['f_score'][i]))
-
-            mae_mean.append(np.mean(utility[T_name]['mae'][i]))
-            mae_std.append(np.std(utility[T_name]['mae'][i]))
-
-            mre_mean.append(np.mean(utility[T_name]['mre'][i]))
-            mre_std.append(np.std(utility[T_name]['mre'][i]))
-
-        if T_name == 'retail':
-            fmt = 'ro-'
-            label = T_name.title()
-        elif T_name == 'BMS1':
-            fmt = 'cs-'
-            label = T_name
-        elif T_name == 'BMS2':
-            fmt = 'b^-'
-            label = T_name
-        elif T_name == 'ml-10M100K':
-            fmt = 'gp-'
-            label = 'Action'
-        elif T_name == 'kosarak':
-            fmt = 'yH-'
-            label = T_name.title()
-
-        ax1.errorbar(r_lst, precision_mean, yerr=precision_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax2.errorbar(r_lst, recall_mean, yerr=recall_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax3.errorbar(r_lst, f_score_mean, yerr=f_score_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax4.errorbar(r_lst, mae_mean, yerr=mae_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax5.errorbar(r_lst, mre_mean, yerr=mre_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-
-    ax1.legend(loc='lower right')
-    ax2.legend(loc='lower right')
-    ax3.legend(loc='lower right')
-    ax4.legend(loc='lower right')
-    ax5.legend(loc='lower right')
-
-    fig1.tight_layout()  # rect=[0, 0.03, 1, 0.95] for suptitle
-    fig2.tight_layout()
-    fig3.tight_layout()
-    fig4.tight_layout()
-    fig5.tight_layout()
-
-    fig1.savefig('..\\reports\\split_rate_vs_precision.svg')
-    fig2.savefig('..\\reports\\split_rate_vs_recall.svg')
-    fig3.savefig('..\\reports\\split_rate_vs_F_score.svg')
-    fig4.savefig('..\\reports\\split_rate_vs_MAE.svg')
-    fig5.savefig('..\\reports\\split_rate_vs_MRE.svg')
-    plt.show()
-
-
-def gen_txt_2(T_names, r_lst, privacy, utility):
-    with open('..\\reports\\split_rate_vs_utility.txt', 'w', newline=''):
-        pass
-    for T_name in T_names:
-        losses =     ['Privacy           ']
-        rs =         ['Utility/Split Rate']
-        precisions = ['Precision         ']
-        recalls =    ['Recall            ']
-        f_scores =   ['F Score           ']
-        maes =       ['MAE               ']
-        mres =       ['MRE               ']
-
-        for i in range(len(r_lst)):
-            rs.append('{:.3f}        '.format(r_lst[i]))
-
-            loss_mean = np.mean(privacy[T_name]['loss'][i])
-            loss_std = np.std(privacy[T_name]['loss'][i])
-            losses.append('{:.3f} ({:.3f})'.format(loss_mean, loss_std))
-
-            precision_mean = np.mean(utility[T_name]['precision'][i])
-            precision_std = np.std(utility[T_name]['precision'][i])
-            precisions.append('{:.3f} ({:.3f})'.format(precision_mean, precision_std))
-
-            recall_mean = np.mean(utility[T_name]['recall'][i])
-            recall_std = np.std(utility[T_name]['recall'][i])
-            recalls.append('{:.3f} ({:.3f})'.format(recall_mean, recall_std))
-
-            f_score_mean = np.mean(utility[T_name]['f_score'][i])
-            f_score_std = np.std(utility[T_name]['f_score'][i])
-            f_scores.append('{:.3f} ({:.3f})'.format(f_score_mean, f_score_std))
-
-            mae_mean = np.mean(utility[T_name]['mae'][i])
-            mae_std = np.std(utility[T_name]['mae'][i])
-            maes.append('{:.3f} ({:.3f})'.format(mae_mean, mae_std))
-
-            mre_mean = np.mean(utility[T_name]['mre'][i])
-            mre_std = np.std(utility[T_name]['mre'][i])
-            mres.append('{:.3f} ({:.3f})'.format(mre_mean, mre_std))
-
-        with open('..\\reports\\split_rate_vs_utility.txt', 'a', newline='') as pu:
-            if T_name == 'retail' or T_name == 'kosarak':
-                pu.write(T_name.title())
-            elif T_name == 'BMS1' or T_name == 'BMS2':
-                pu.write(T_name)
-            elif T_name == 'ml-10M100K':
-                pu.write('Action')
-            pu.write('\n')
-            pu.write('  '.join(losses))
-            pu.write('\n')
-            pu.write('  '.join(rs))
-            pu.write('\n')
-            pu.write('  '.join(precisions))
-            pu.write('\n')
-            pu.write('  '.join(recalls))
-            pu.write('\n')
-            pu.write('  '.join(f_scores))
-            pu.write('\n')
-            pu.write('  '.join(maes))
-            pu.write('\n')
-            pu.write('  '.join(mres))
-            pu.write('\n\n')
-
-
-def plot_metrics_3(T_names, phi_lst, utility):
-    fig1, ax1 = plt.subplots(figsize=(6, 6))
-    fig2, ax2 = plt.subplots(figsize=(6, 6))
-    fig3, ax3 = plt.subplots(figsize=(6, 6))
-    fig4, ax4 = plt.subplots(figsize=(6, 6))
-    fig5, ax5 = plt.subplots(figsize=(6, 6))
-
-    ax1.set_xlabel('Maximum Support Difference')
-    ax1.set_ylabel('Precision')
-    ax1.set_xlim(0.1, 1)
-    ax1.set_ylim(0.0, 1)
-    ax1.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax1.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax1.grid(True, ls='--')
-
-    ax2.set_xlabel('Maximum Support Difference')
-    ax2.set_ylabel('Recall')
-    ax2.set_xlim(0.1, 1)
-    ax2.set_ylim(0, 1)
-    ax2.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax2.grid(True, ls='--')
-
-    ax3.set_xlabel('Maximum Support Difference')
-    ax3.set_ylabel('F Score')
-    ax3.set_xlim(0.1, 1)
-    ax3.set_ylim(0, 1)
-    ax3.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax3.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax3.grid(True, ls='--')
-
-    ax4.set_xlabel('Maximum Support Difference')
-    ax4.set_ylabel('MAE')
-    ax4.set_xlim(0.1, 1)
-    ax4.set_ylim(0, 0.025)
-    ax4.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax4.yaxis.set_major_locator(ticker.MultipleLocator(0.0025))
-    ax4.grid(True, ls='--')
-
-    ax5.set_xlabel('Maximum Support Difference')
-    ax5.set_ylabel('MRE')
-    ax5.set_xlim(0.1, 1)
-    ax5.set_ylim(0, 0.15)
-    ax5.xaxis.set_major_locator(ticker.MultipleLocator(0.1))
-    ax5.yaxis.set_major_locator(ticker.MultipleLocator(0.015))
-    ax5.grid(True, ls='--')
-
-    for T_name in T_names:
-        precision_mean, precision_std = [], []
-        recall_mean, recall_std = [], []
-        f_score_mean, f_score_std = [], []
-
-        mae_mean, mae_std = [], []
-        mre_mean, mre_std = [], []
-
-        for i in range(len(phi_lst)):
-            precision_mean.append(np.mean(utility[T_name]['precision'][i]))
-            precision_std.append(np.std(utility[T_name]['precision'][i]))
-
-            recall_mean.append(np.mean(utility[T_name]['recall'][i]))
-            recall_std.append(np.std(utility[T_name]['recall'][i]))
-
-            f_score_mean.append(np.mean(utility[T_name]['f_score'][i]))
-            f_score_std.append(np.std(utility[T_name]['f_score'][i]))
-
-            mae_mean.append(np.mean(utility[T_name]['mae'][i]))
-            mae_std.append(np.std(utility[T_name]['mae'][i]))
-
-            mre_mean.append(np.mean(utility[T_name]['mre'][i]))
-            mre_std.append(np.std(utility[T_name]['mre'][i]))
-
-        if T_name == 'retail':
-            fmt = 'ro-'
-            label = T_name.title()
-        elif T_name == 'BMS1':
-            fmt = 'cs-'
-            label = T_name
-        elif T_name == 'BMS2':
-            fmt = 'b^-'
-            label = T_name
-        elif T_name == 'ml-10M100K':
-            fmt = 'gp-'
-            label = 'Action'
-        elif T_name == 'kosarak':
-            fmt = 'yH-'
-            label = T_name.title()
-
-        ax1.errorbar(phi_lst, precision_mean, yerr=precision_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax2.errorbar(phi_lst, recall_mean, yerr=recall_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax3.errorbar(phi_lst, f_score_mean, yerr=f_score_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax4.errorbar(phi_lst, mae_mean, yerr=mae_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-        ax5.errorbar(phi_lst, mre_mean, yerr=mre_std, fmt=fmt, fillstyle='none', label=label, capsize=5)
-
-    ax1.legend(loc='lower right')
-    ax2.legend(loc='lower right')
-    ax3.legend(loc='lower right')
-    ax4.legend(loc='lower right')
-    ax5.legend(loc='lower right')
-
-    fig1.tight_layout()  # rect=[0, 0.03, 1, 0.95] for suptitle
-    fig2.tight_layout()
-    fig3.tight_layout()
-    fig4.tight_layout()
-    fig5.tight_layout()
-
-    fig1.savefig('..\\reports\\maximum_support_difference_vs_precision.svg')
-    fig2.savefig('..\\reports\\maximum_support_difference_vs_recall.svg')
-    fig3.savefig('..\\reports\\maximum_support_difference_vs_F_score.svg')
-    fig4.savefig('..\\reports\\maximum_support_difference_vs_MAE.svg')
-    fig5.savefig('..\\reports\\maximum_support_difference_vs_MRE.svg')
-    plt.show()
-
-
-def gen_txt_3(T_names, phi_lst, privacy, utility):
     with open('..\\reports\\maximum_support_difference_vs_utility.txt', 'w', newline=''):
         pass
     for T_name in T_names:
